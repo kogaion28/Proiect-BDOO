7de4d962a38e8434e32aa80019ea0f07eb5e390a
Jude Bogdan Laurentiu <43954492+kogaion28@users.noreply.github.com>
2020-05-31 16:47:56 +0300

    Add files via upload

--- pacient.py
+++ pacient.py
@@ -1,6 +1,9 @@
 from tkinter import *
 import sqlite3,sys
 
+
+
+#conexiunea cu baza de date 
 def connection():
     try:
         conn=sqlite3.connect("pacient.db")
@@ -9,8 +12,9 @@ def connection():
     return conn    
 
 
+#verificare 
 def verifier():
-    a=b=c=d=e=f=0
+    a=b=c=d=e=f=g=h=i=j=k=0
     if not pacient_nume.get():
         t1.insert(END,"<>Numele pacientului<>\n")
         a=1
@@ -29,24 +33,42 @@ def verifier():
     if not ADRESA.get():
         t1.insert(END,"<>Adresa<>\n")
         f=1
-    if a==1 or b==1 or c==1 or d==1 or e==1 or f==1:
+    if not GRUPA.get():
+        t1.insert(END,"<>Grupa<>\n")
+        g=1
+    if not STARE.get():
+        t1.insert(END,"<>Stare<>\n")
+        h=1
+    if not INTERNARI.get():
+        t1.insert(END,"<>Internari<>\n")
+        i=1
+    if not DATA.get():
+        t1.insert(END,"<>Internari<>\n")
+        j=1
+    if not ORA.get():
+        t1.insert(END,"<>Internari<>\n")
+        k=1
+    
+
+    
+    if a==1 or b==1 or c==1 or d==1 or e==1 or f==1 or g==1 or h==1 or i==1 or j==1 or k==1:
         return 1
     else:
         return 0
 
-
+#Adauga pacient
 def add_student():
             ret=verifier()
             if ret==0:
                 conn=connection()
                 cur=conn.cursor()
-                cur.execute("CREATE TABLE IF NOT EXISTS PACIENT(NUME TEXT,NR INTEGER,AFECTIUNE TEXT,TELEFON INTEGER,TATA TEXT,ADRESA TEXT)")
-                cur.execute("insert into PACIENT values(?,?,?,?,?,?)",(pacient_nume.get(),int(NR.get()),AFECTIUNE.get(),int(TELEFON.get()),TATA.get(),ADRESA.get()))
+                cur.execute("CREATE TABLE IF NOT EXISTS PACIENT(NUME TEXT,NR INTEGER,AFECTIUNE TEXT,TELEFON INTEGER,TATA TEXT,ADRESA TEXT,GRUPA TEXT,STARE TEXT,INTERNARI TEXT,DATA INTEGER,ORA INTEGER)")#creaza baza de data daca nu exista
+                cur.execute("insert into PACIENT values(?,?,?,?,?,?,?,?,?,?,?)",(pacient_nume.get(),int(NR.get()),AFECTIUNE.get(),int(TELEFON.get()),TATA.get(),ADRESA.get(),GRUPA.get(),STARE.get(),INTERNARI.get(),DATA.get(),ORA.get()))
                 conn.commit()
                 conn.close()
                 t1.insert(END,"ADAUGARE CU SUCCES!!!\n")
 
-
+#Afiseaza pacienti
 def view_student():
     conn=connection()
     cur=conn.cursor()
@@ -56,7 +78,7 @@ def view_student():
     for i in data:
         t1.insert(END,str(i)+"\n")
 
-
+#Sterge pacient
 def delete_student():
     ret=verifier()
     if ret==0:
@@ -66,13 +88,13 @@ def delete_student():
         conn.commit()
         conn.close()
         t1.insert(END,"PACIENT STERS CU SUCCES!!!\n")
-
+#Actualizaeaza  pacient
 def update_student():
     ret=verifier()
     if ret==0:
         conn=connection()
         cur=conn.cursor()
-        cur.execute("UPDATE PACIENT SET NUME=?,NR=?,AFECTIUNE=?,TELEFON=?,TATA=?,ADRESA=? where NR=?",(pacient_nume.get(),int(NR.get()),AFECTIUNE.get(),int(TELEFON.get()),TATA.get(),ADRESA.get(),int(NR.get())))
+        cur.execute("UPDATE PACIENT SET NUME=?,NR=?,AFECTIUNE=?,TELEFON=?,TATA=?,ADRESA=?,GRUPA=?,STARE=?,INTERNARI=?,DATA=?,ORA=? where NR=?",(pacient_nume.get(),int(NR.get()),AFECTIUNE.get(),int(TELEFON.get()),TATA.get(),ADRESA.get(),GRUPA.get(),STARE.get(),INTERNARI.get(),DATA.get(),ORA.get(),int(NR.get())))     #actualizeaza pacient dupa
         conn.commit()
         conn.close()
         t1.insert(END,"Actualizare cu succes!!!\n")
@@ -85,14 +107,19 @@ def clse():
 if __name__=="__main__":
     root=Tk()
     root.title("Gestiunea pacientilor")
-     
+#variabile
     pacient_nume=StringVar()
     NR=StringVar()
     AFECTIUNE=StringVar()
     TELEFON=StringVar()
     TATA=StringVar()
     ADRESA=StringVar()
-    
+    GRUPA=StringVar()
+    STARE=StringVar()
+    INTERNARI=StringVar()
+    DATA=StringVar()
+    ORA=StringVar()
+#lebel
     label1=Label(root,text="Numele Pacientului:",bg="red")
     label1.place(x=0,y=0)
 
@@ -111,6 +138,22 @@ if __name__=="__main__":
     label6=Label(root,text="Adresa:",bg="red")
     label6.place(x=0,y=150)
 
+    label7=Label(root,text="Grupa:",bg="red")
+    label7.place(x=0,y=180)
+
+    label8=Label(root,text="Stare de sanatate:",bg="red")
+    label8.place(x=0,y=210)
+
+    label9=Label(root,text="Internari la spital:",bg="red")
+    label9.place(x=0,y=240)
+
+    label10=Label(root,text="Data programari:",bg="red")
+    label10.place(x=0,y=270)
+
+    label11=Label(root,text="Ora programari:",bg="red")
+    label11.place(x=0,y=300)
+
+#casuta
     e1=Entry(root,textvariable=pacient_nume , bg="LightGreen")
     e1.place(x=150,y=0)
 
@@ -128,12 +171,30 @@ if __name__=="__main__":
 
     e6=Entry(root,textvariable=ADRESA,bg="LightGreen")
     e6.place(x=150,y=150)
+
+    e7=Entry(root,textvariable=GRUPA,bg="LightGreen")
+    e7.place(x=150,y=180)
+
+    e8=Entry(root,textvariable=STARE,bg="LightGreen")
+    e8.place(x=150,y=210)
+
+    e9=Entry(root,textvariable=INTERNARI,bg="LightGreen")
+    e9.place(x=150,y=240)
+
+    e10=Entry(root,textvariable=DATA,bg="LightGreen")
+    e10.place(x=150,y=270)
+
+    e11=Entry(root,textvariable=ORA,bg="LightGreen")
+    e11.place(x=150,y=300)
+    
+    #t1=Text(root,width=90,height=30,bg="LightBlue")
+   # t1.grid(row=10,column=1)
     
     t1=Text(root,width=90,height=30,bg="LightBlue")
     t1.grid(row=10,column=1)
    
 
-
+#butoane
     b1=Button(root,text="ADAUGA PACIENT",command=add_student,width=40,bg="blue")
     b1.grid(row=11,column=0)
 
